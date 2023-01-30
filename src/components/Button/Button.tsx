ba96b5810062e6353f3c971981a52f09f973e2a8
Florent Barth <flrnt.barth@gmail.com>
2023-01-30 18:24:54 +0000

    add styled-components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,38 @@
 import React from "react";
-import "./button.css";
+import styled from "styled-components";
 
+const ButtonStyled = styled.button`
+  &.storybook-button {
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 3em;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+  }
+  &.storybook-button--primary {
+    color: white;
+    background-color: #1ea7fd;
+  }
+  &.storybook-button--secondary {
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  }
+  &.storybook-button--small {
+    font-size: 12px;
+    padding: 10px 16px;
+  }
+  &.storybook-button--medium {
+    font-size: 14px;
+    padding: 11px 20px;
+  }
+  &.storybook-button--large {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
+`;
 export interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
@@ -38,7 +70,7 @@ const Button = ({
     ? "storybook-button--primary"
     : "storybook-button--secondary";
   return (
-    <button
+    <ButtonStyled
       type="button"
       className={["storybook-button", `storybook-button--${size}`, mode].join(
         " "
@@ -47,7 +79,7 @@ const Button = ({
       onClick={onClick}
     >
       {label}
-    </button>
+    </ButtonStyled>
   );
 };
 
