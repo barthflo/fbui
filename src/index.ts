1eec1abd19035dd118b7223b8d0aa3c35082331f
Florent Barth <flrnt.barth@gmail.com>
2023-02-18 13:27:34 +0000

    downgrade styled-components version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import Button from "./components/Button";
+import Button from "./components/Button";
 import FlexBox from "./components/FlexBox";
 
-export { FlexBox };
+export { Button, FlexBox };
